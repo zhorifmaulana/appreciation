330427b3b9e9650215a619f2c2888de37ad1a5ef
zhorifmaulana <zhorif.maulana@ui.ac.id>
2020-03-01 15:59:46 +0700

    using react

--- src/Pages/Afeksi.jsx
+++ src/Pages/Afeksi.jsx
@@ -4,7 +4,7 @@ import '../App.css';
 const Afeksi = () => (
 <div>
 <p>Halo!</p>
-<p>Selamat sudah menempuh sebagian tantangan dalam hidup! Anda kuat dan saya yakin anda akan terus bisa berkembang lagi. Seorang teman pernah berkata bahwa jabatan politik seperti Ketua BEM hingga kepala departemen sekosong-kosongnya pasti akan ada yang mengisi. Tetapi tidak untuk menjadi Project Officer.</p>
+<p>Selamat sudah menempuh sebagian tantangan dalam hidup! Anda kuat dan saya yakin anda akan terus bisa berkembang lagi.</p>
 </div>
 );
 
